67855ad4239d320205683183b533e5348a039598
Timo Früh <timo.frueh@icloud.com>
2022-10-30 16:39:53 +0100

    fix type inconsistency

--- src/coord-conv.cpp
+++ src/coord-conv.cpp
@@ -2,24 +2,24 @@
 #include "../headers/coord-conv.h"
 
 wgs84 lv95ToWgs84(const lv95& input_coord) {
-	double y_prime = (input_coord.e - 2600000) / 1000000;
-	double x_prime = (input_coord.n - 1200000) / 1000000;
+	double y_prime = (input_coord.e - 2600000.0) / 1000000.0;
+	double x_prime = (input_coord.n - 1200000.0) / 1000000.0;
 
 	double lambda_prime = 2.6779094
 		+ 4.728982 * y_prime
 		+ 0.791484 * y_prime * x_prime
-		+ 0.1306 * y_prime * pow(x_prime, 2)
-		- 0.0436 * pow(y_prime, 3);
+		+ 0.1306 * y_prime * pow(x_prime, 2.0)
+		- 0.0436 * pow(y_prime, 3.0);
 
 	double phi_prime = 16.9023892
 		+ 3.238272 * x_prime
-		- 0.270978 * pow(y_prime, 2)
-		- 0.002528 * pow(x_prime, 2)
-		- 0.0447 * pow(y_prime, 2) * x_prime
-		- 0.0140 * pow(x_prime, 3);
+		- 0.270978 * pow(y_prime, 2.0)
+		- 0.002528 * pow(x_prime, 2.0)
+		- 0.0447 * pow(y_prime, 2.0) * x_prime
+		- 0.0140 * pow(x_prime, 3.0);
 
-	double lambda = lambda_prime * 100 / 36;
-	double phi = phi_prime * 100 / 36;
+	double lambda = lambda_prime * 100.0 / 36.0;
+	double phi = phi_prime * 100.0 / 36.0;
 
 	wgs84 result;
 	result.e = lambda;
