7731b76f1393369d96896081f5fc43dffc8b0248
Timo Früh <timo.frueh@icloud.com>
2022-11-04 22:53:59 +0100

    implement CLI11

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include "../lib/CLI11.hpp"
 #include "../headers/coord-conv.h"
 
-int main() {
+int main(int argc, char** argv) {
+	CLI::App app{"Convert LV95 coordinates to WGS84"};
+	
 	lv95 input;
+	app.add_option<double>("-e,--east", input.e, "Easting coordinate (LV95)")->required();
+	app.add_option<double>("-n,--north", input.n, "Northing coordinate (LV95)")->required();
 
-	std::cout << "Enter E (LV95): ";
-	std::cin >> input.e;
-
-	std::cout << "Enter N (LV95): ";
-	std::cin >> input.n;
+	CLI11_PARSE(app, argc, argv);
 
 	wgs84 output;
 	output = lv95ToWgs84(input);
