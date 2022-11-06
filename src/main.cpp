1eaedc119216f0454454410b6a45ace6d747ab22
Timo Früh <timo.frueh@icloud.com>
2022-11-06 16:39:04 +0100

    implement vice-versa function

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,31 @@
 #include "../headers/coord-conv.h"
 
 int main(int argc, char** argv) {
-	CLI::App app{"Convert LV95 coordinates to WGS84"};
+	CLI::App app("Convert LV95 coordinates to WGS84 and vice-versa");
+
+	CLI::App* lv95_scmd = app.add_subcommand("lv95", "convert LV95 to WGS84");
+	CLI::App* wgs84_scmd = app.add_subcommand("wgs84", "convert WGS84 to LV95");
+	app.require_subcommand(1, 1);
 	
-	lv95 input;
-	app.add_option<double>("-e,--east", input.e, "Easting coordinate (LV95)")->required();
-	app.add_option<double>("-n,--north", input.n, "Northing coordinate (LV95)")->required();
+	lv95 lv95_input;
+	lv95_scmd->add_option<double>("-e,--east", lv95_input.e, "Easting coordinate (LV95)")->required();
+	lv95_scmd->add_option<double>("-n,--north", lv95_input.n, "Northing coordinate (LV95)")->required();
+
+	wgs84 wgs84_input;
+	wgs84_scmd->add_option<double>("-e,--east", wgs84_input.e, "Easting coordinate in WGS84")->required();
+	wgs84_scmd->add_option<double>("-n,--north", wgs84_input.n, "Northing coordinate in WGS84")->required();
 
 	CLI11_PARSE(app, argc, argv);
 
-	wgs84 output;
-	output = lv95ToWgs84(input);
-
-	std::cout << "\nCoordinates: N " << output.n << ", E " << output.e << "\n";
+	if (*lv95_scmd) {
+		wgs84 output;
+		output = lv95ToWgs84(lv95_input);
+		std::cout << "\nCoordinates in WGS84: N " << output.n << ", E " << output.e << "\n";
+	} else {
+		lv95 output;
+		output = wgs84ToLv95(wgs84_input);
+		std::cout << "\nCoordinates in LV95: E " << output.e << ", N " << output.n << "\n";
+	}
 
 	return 0;
 }
